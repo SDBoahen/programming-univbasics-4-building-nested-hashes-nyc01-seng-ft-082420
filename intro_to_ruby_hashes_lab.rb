220007a742d597b2021ad455a817b15d87fc8f65
Darkwah Boahen (Sam) <sdboahen@gmail.com>
2020-08-17 02:15:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -89,39 +89,6 @@ def
   
   
   monopoly =
-    {
-    
-    
-    :railroads =>
-    {
-    
-      :pieces => 4,
-    
-      :rent_in_dollars =>
-      {
-        :one_piece_owned => 25,
-        :two_pieces_owned => 50,
-        :three_pieces_owned => 100,
-        :four_pieces_owned => 200
-      },
-    
-      :names =>
-      {
-        :reading_railroad => { },
-        :pennsylvania_railroad => { },
-        :b_and_o_railroad => { },
-        :shortline => { }
-      }
-      
-    }
-    
-    
-  }
-  
-  
-  
-  XX-
-  
   {
   :railroads =>
   {
@@ -157,7 +124,9 @@ def
       }
       
     }
+    
   }
+  
 }
 
 
